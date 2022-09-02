72884e4e9e120773aeaf5c834ba0543efff3b26e
Junya <61687637+newJuniorV@users.noreply.github.com>
2022-09-02 19:29:34 +0900

    new

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -3,11 +3,11 @@ import { Card } from "../../atoms/card/Card";
 import { UserIconWithName } from "../../molocules/user/UserIconwithNama";
 
 export const UserCard = (props) => {
-  const { user, isAdmin } = props;
+  const { user } = props;
 
   return (
     <Card>
-      <UserIconWithName image={user.image} name={user.name} isAdmin={isAdmin} />
+      <UserIconWithName image={user.image} name={user.name} />
       <SDl>
         <dt>mail</dt>
         <dd>{user.email}</dd>
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,7 +1,9 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { SearchInput } from "../molocules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
-import { useLocation } from "react-router-dom";
+import { UserContext } from "../providers/UserProvider";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -18,16 +20,18 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  const isAdmin = state ? state.isAdmin : false;
+  const { userInfo, setUserInfo } = useContext(UserContext);
 
+  const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>change</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
-          <UserCard key={user.id} user={user} isAdmin={isAdmin} />
+          <UserCard key={user.id} user={user} />
         ))}
       </SUserArea>
     </SContainer>
